854fdc91c0b2c50f096ac9f2ba167085206048c2
Mr Hanzla <mr.hanzla101@gmail.com>
2024-02-10 15:49:19 +0500

    add router for cart items and detail product view

--- src/router.js
+++ src/router.js
@@ -1,13 +1,37 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from './components/Login.vue';
-import ApiTesting from './components/ApiTesting.vue';
 import Products from './components/Products.vue'
+import ProductDetail from './components/ProductDetail.vue';
 import ProductDetails from './components/ProductDetails.vue'
+import Cart from './components/Cart.vue';
 
 const routes = [
-    { path: '/', component: Products },
-    { path: '/login', component: Login },
-    { path: '/product-details', component: ProductDetails, props: true },
+    {
+        path: '/',
+        name: 'Home',
+        component: Products
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: Login
+    },
+    {
+        path: '/product/:id',
+        name: 'ProductDetail',
+        component: ProductDetail
+    },
+    {
+        path: '/cart',
+        name: 'CartView',
+        component: Cart
+    },
+    {
+        path: '/product-details',
+        name: 'ProductDetails',
+        component: ProductDetails,
+        props: true
+    },
 ]
 
 const router = createRouter({
